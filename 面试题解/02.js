5ef9111b5f459d9bfe08515adb2e8b4141aa03be
Davidyang <qq244961759@163.com>
2019-08-26 14:54:31 +0800

    面试题解

--- 面试题解/02.js
+++ 面试题解/02.js
@@ -1,24 +1,29 @@
 class Scheduler {
+  /**
+   * tasks 异步任务池
+   * taskAction 当前在整执行的异步任务数量
+   * concurrent 并发 阈值， 在浏览器环境下， 最多并发值应该为 6；
+   */
   constructor() {
     this.tasks = [];
     this.taskAction = 0;
     this.concurrent = 2;
   }
   add(promiseCreator) {
-    return new Promise(resolve =>{
+    return new Promise(resolve => {
       this.tasks.push(() => promiseCreator().then(resolve));
       this.runTask();
-    })
+    });
   }
   runTask() {
     if (this.taskAction >= this.concurrent) return;
     const currentTask = this.tasks.shift();
-    this.taskAction ++;
-    if(currentTask) {
-      currentTask().then(()=>{
-        this.taskAction -=1;
-        this.runTask()
-      })
+    this.taskAction++;
+    if (currentTask) {
+      currentTask().then(() => {
+        this.taskAction -= 1;
+        this.runTask();
+      });
     }
   }
 }
@@ -37,5 +42,3 @@ addTask(300, "3");
 addTask(400, "4");
 
 // output: 2 3 1 4
-
-
