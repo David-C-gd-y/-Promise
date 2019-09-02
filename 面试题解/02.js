98d3f67c97baaefd86980e60a80ac764cec9e6eb
David.y <qq244961759@.163.com>
2019-09-02 11:37:27 +0800

    123

--- 面试题解/02.js
+++ 面试题解/02.js
@@ -1,40 +1,41 @@
 class Scheduler {
-  /**
-   * tasks 异步任务池
-   * taskAction 当前在整执行的异步任务数量
-   * concurrent 并发 阈值， 在浏览器环境下， 最多并发值应该为 6；
-   */
-  constructor() {
-    this.tasks = [];
-    this.taskAction = 0;
-    this.concurrent = 2;
-  }
-  add(promiseCreator) {
-    return new Promise(resolve => {
-      this.tasks.push(() => promiseCreator().then(resolve));
-      this.runTask();
-    });
-  }
-  runTask() {
-    if (this.taskAction >= this.concurrent) return;
-    const currentTask = this.tasks.shift();
-    this.taskAction++;
-    if (currentTask) {
-      currentTask().then(() => {
-        this.taskAction -= 1;
-        this.runTask();
-      });
+    123123
+    /**
+     * tasks 异步任务池
+     * taskAction 当前在整执行的异步任务数量
+     * concurrent 并发 阈值， 在浏览器环境下， 最多并发值应该为 6；
+     */
+    constructor() {
+        this.tasks = [];
+        this.taskAction = 0;
+        this.concurrent = 2;
     }
-  }
+    add(promiseCreator) {
+        return new Promise(resolve => {
+            this.tasks.push(() => promiseCreator().then(resolve));
+            this.runTask();
+        });
+    }
+    runTask() {
+        if (this.taskAction >= this.concurrent) return;
+        const currentTask = this.tasks.shift();
+        this.taskAction++;
+        if (currentTask) {
+            currentTask().then(() => {
+                this.taskAction -= 1;
+                this.runTask();
+            });
+        }
+    }
 }
 const timeout = timer => new Promise(resolve => setTimeout(resolve, timer));
 const scheduler = new Scheduler();
 const addTask = (time, order) => {
-  scheduler
-    .add(() => timeout(time))
-    .then(() => {
-      console.log(order);
-    });
+    scheduler
+        .add(() => timeout(time))
+        .then(() => {
+            console.log(order);
+        });
 };
 addTask(1000, "1");
 addTask(500, "2");
