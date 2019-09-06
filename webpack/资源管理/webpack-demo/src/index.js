084c7053717bb1c4780030b42cd938121eb6d032
David.y <qq244961759@.163.com>
2019-09-06 10:16:43 +0800

    资源管理

--- webpack/资源管理/webpack-demo/src/index.js
+++ webpack/资源管理/webpack-demo/src/index.js
@@ -1,8 +1,17 @@
 import _ from 'lodash'
+import './style.css'
+import Img from './test.jpeg'
+import Data from './data.xml';
 function component () {
   let el = document.createElement('div')
 
-  el.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  el.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  el.classList.add('hello');
+  // 添加图片到div
+  var img = new Image();
+  img.src = Img;
+  el.append(img);
+  console.log(Data);
   return el
 }
 document.body.appendChild(component())
