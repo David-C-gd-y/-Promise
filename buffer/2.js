fbffb4c96dc169511851344f419af67fcc070e37
Davie.yang <qq244961759@163.com>
2020-04-28 20:23:03 +0800

    Buffer 常用API 熟悉

--- buffer/2.js
+++ buffer/2.js
@@ -29,3 +29,46 @@ let buf = Buffer.allocUnsafe(6);
 // console.log(buf.readInt8(2));//32
 // console.log(buf.readInt8(3));//48
 
+/**
+ * Little-Endian & Big-Endian
+ *
+ * 不同的cpu有不同的字节序类型， 这些字节序是指整数在内存中保存的顺序
+ * @ Big-Endian: 将高序字节存储在起始位置 （高位编址）
+ * @ Little-endian：将低序字节存储在起始地址（低位编址）
+ * 计算机电路先处理低位字节，效率比较高，因为计算都是从低位开始的
+ */
+
+//  let buffer = Buffer.alloc(4);
+//  buffer.writeInt16BE(2**8,0);
+//  console.log(buffer) // <Buffer 01 00 00 00>
+//  console.log(buffer.readInt16BE(0))// 256
+
+//  buffer.writeInt16LE(2**8, 2)
+//  console.log(buffer) // <Buffer 01 00 00 00>
+//  console.log(buffer.readInt16LE(2))// 256
+
+ // toString() 方法
+//  let buffer = Buffer.from('我是一个buffer');
+// buf.toString([encoding[, start[, end]]])
+//  console.log(buffer.toString('utf8', 0,4)) // 中文 是3位
+// let subBuffer = buffer.slice();// 和数组那个 一样
+// console.log(subBuffer.toString())
+// let {StringDecoder}  = require('string_decoder');
+// let sd = new StringDecoder();
+// console.log(sd.write(buffer.slice(0,4)));
+// console.log(sd.write(buffer.slice(4)));
+// console.log(sd)
+
+
+// copy 
+// let subBuffer = Buffer.alloc(6);
+
+// buffer.copy(subBuffer, 0, 0,3)
+// buffer.copy(subBuffer, 3, 3,6)
+// console.log(subBuffer.toString())
+
+// concat
+let buffer1 = Buffer.from('大卫');
+let buffer2 = Buffer.from('很帅');
+let buffer = Buffer.concat([buffer1,buffer2]);
+console.log(buffer.toString());
